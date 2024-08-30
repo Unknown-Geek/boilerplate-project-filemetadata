5f0f4cd75ab6ac7aae20473be5f40e1f727236f6
Shravan Nander Pandala <97742905+Unknown-Geek@users.noreply.github.com>
2024-08-30 17:40:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +14,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse',upload.single('upfile'), (req,res) => {
+  try {
+      console.log(req.file);
+      if (req.file) {
+        res.json({
+          name: req.file.originalname,
+          type: req.file.mimetype,
+          size: req.file.size
+        });
+      } else {
+        res.json({error : "No file found"});
+      }
+  } catch (err) {
+        console.error(err);
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
